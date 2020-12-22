57483b0eca50c6dafb3cbe13b0f25c64c0106b51
Selim Saygi <saygiselim@gmail.com>
2020-12-22 20:05:38 +0100

    update demo

--- src/app/pages/video-player-page/video-player-page.component.ts
+++ src/app/pages/video-player-page/video-player-page.component.ts
@@ -18,9 +18,19 @@ export class VideoPlayerPageComponent implements OnInit {
 
         this.videoSources = [
             {
-                title: 'Sintel',
-                source: 'http://media.w3.org/2010/05/sintel/trailer.mp4',
-                poster: 'http://media.w3.org/2010/05/sintel/poster.png',
+                title: 'Big Buck Bunny - 2008',
+                poster: 'https://www.blender.org/wp-content/uploads/2013/07/poster-bigbuckbunny.jpg',
+                source: 'https://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_stereo.ogg'
+            },
+            {
+                title: 'Sintel - 2010',
+                poster: 'https://www.blender.org/wp-content/uploads/2013/07/poster-sintel.jpg',
+                source: 'http://ftp.nluug.nl/pub/graphics/blender/demo/movies/Sintel.2010.1080p.mkv'
+            },
+            {
+                title: 'Tears of Steel - 2012',
+                poster: 'https://www.blender.org/wp-content/uploads/2013/07/poster-tearsofsteel.jpg',
+                source: 'http://media.xiph.org/mango/tears_of_steel_1080p.webm'
             }
         ];
     }
